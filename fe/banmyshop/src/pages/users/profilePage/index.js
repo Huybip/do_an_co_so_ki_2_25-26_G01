b70e889c64f4063bfbc1052a2a00dfeb05a87b08
Huy <huytranhh3@gmail.com>
2026-02-26 18:11:22 +0700

    update

--- fe/banmyshop/src/pages/users/profilePage/index.js
+++
@@ -1,7 +0,0 @@
-import {memo} from "react"; //PureComponent cho Function Component
-
-const ProfilePage = () => {
-    return <h1>ProfilePage</h1>;
-};
-
-export default memo(ProfilePage); //tránh render lại khi props không thay đổi
--- fe/banmyshop/src/utils/formatter.js
+++
@@ -1,7 +0,0 @@
-//format đơn vị tiền VND
-export const formatter = (number) => {
-  return new Intl.NumberFormat("vi-VN", {
-    style: "currency",
-    currency: "VND"
-  }).format(number);
-};
